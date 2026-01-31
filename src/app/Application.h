67ce8c6c9e53b168040b2f0f7488e588392dd8e8
Honey <khongorbe5002@gmail.com>
2026-01-31 00:35:25 -0800

    tried a viewer thing im not really sure whats happening

--- src/app/Application.h
+++ src/app/Application.h
@@ -1,8 +1,18 @@
 #pragma once
+#include <memory> // For std::unique_ptr
+
+// Forward declaration (tells the compiler "this class exists")
+// This is better than #include because it keeps compile times fast.
+namespace Render { class RenderController; }
 
 class Application
 {
 public:
     Application();
+    ~Application(); // Destructor is required now for unique_ptr
     void run();
+
+    private:
+    // Application owns the Renderer
+    std::unique_ptr<Render::RenderController> m_renderController;
 };
--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,45 @@
 #include <QApplication>
 #include "app/Application.h"
 
+//added by honey
+#include "render/RenderController.h"
+#include "render/viewer.h" 
+#include "render/viewerInteractor.h" 
+#include <BRepTools.hxx>
+#include <BRepPrimAPI_MakeBox.hxx>
+
+
+
 int main(int argc, char *argv[])
 {
+    /* 
     QApplication app(argc, argv);
     Application application;
     application.run();
     return app.exec();
+    */
+    
+    Viewer vout(50, 50, 500, 500);
+
+  vout << BRepPrimAPI_MakeBox(10, 10, 20)
+       << BRepPrimAPI_MakeBox(20, 30, 10);
+
+  if ( argc > 1 )
+  {
+    BRep_Builder bb;
+    TopoDS_Shape fromFile;
+    //
+    if ( !BRepTools::Read(fromFile, argv[1], bb) )
+    {
+      std::cout << "Failed to read BREP shape from file " << argv[1] << std::endl;
+      return 1;
+    }
+
+    vout << fromFile;
+  }
+
+  vout.StartMessageLoop();
+
+  return 0;
+
 }
