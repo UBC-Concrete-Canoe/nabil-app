24a781409325d48f873821c5d95023fcfb4dd598
Nabil-Alhanif <nabil.alhanif@protonmail.com>
2026-02-03 21:26:14 -0800

    Force X11/XWayland on linux

--- src/main.cpp
+++ src/main.cpp
@@ -2,8 +2,18 @@
 #include "app/Application.h"
 #include "ui/MainWindow.h"
 
+// Linux specific
+#ifdef Q_OS_LINUX
+#include <cstdlib>
+#endif
+
 int main(int argc, char *argv[])
 {
+
+    // OpenCascade does not work under wayland, need to force X11/XWayland
+#ifdef Q_OS_LINUX
+    setenv("QT_QPA_PLATFORM", "xcb", 1);
+#endif
 
     QApplication app(argc, argv);
     Application application;
