bfe8ea5fe8feace5e999290ffd7cad89bb07f7be
solsticefox <rex.eric.xu@gmail.com>
2026-03-08 23:03:27 -0700

    Cleaned up control point, fully implemented control point

--- src/core/ControlPoint.cpp
+++
@@ -1,9 +0,0 @@
-#include "ControlPoint.h"
-
-ControlPoint::ControlPoint(int id, const gp_Pnt& pos, double weight)
-  : m_id(id)
-  , m_position(pos)
-  , m_weight(weight)
-  , m_isDirty(false)
-{
-}
